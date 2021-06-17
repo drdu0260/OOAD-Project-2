72b0fffaf69fb57f4adbecdb3432851b3dd0c410
drdu0260 <47010836+drdu0260@users.noreply.github.com>
2021-06-16 20:41:57 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,7 @@ class Main {
         PrintStream console = System.out;
         // Assign o to output stream
         System.setOut(o);
+        
         for(int i=1; i<=days; i++){
             mech.clockIn(i);
             //for loop that runs through garage array and does work on each vehicle
@@ -37,7 +38,7 @@ class Main {
         }
         // Use stored value for output stream
         System.setOut(console);
-        //Lines 18-23,37 from https://www.geeksforgeeks.org/redirecting-system-out-println-output-to-a-file-in-java/
+        //Lines 18-23,40 from https://www.geeksforgeeks.org/redirecting-system-out-println-output-to-a-file-in-java/
         
     }
 }
